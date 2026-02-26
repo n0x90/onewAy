0fec2be626fa7f9de06944afb2fae8935afb3eb0
n0x90 <t.morgan42@icloud.com>
2026-02-26 05:41:59 +0000

    Add HTTPS to vite frontend

--- server/frontend/vite.config.ts
+++ server/frontend/vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import fs from 'node:fs';
+import path from 'node:path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: 'localhost',
+    port: 5173,
+    https: {
+      key: fs.readFileSync(
+        path.resolve(process.env.HOME ?? '', '.onewAy/onewAy.key'),
+      ),
+      cert: fs.readFileSync(
+        path.resolve(process.env.HOME ?? '', '.onewAy/onewAy.crt'),
+      ),
+    },
+  },
 });
